8a26ab16807bbda5f938c679156d8d4c7736cb6d
jluby <john.luby@chicagobooth.edu>
2022-03-29 20:37:23 -0500

    Whoops again

--- src/task_tracker/lst.py
+++ src/task_tracker/lst.py
@@ -112,7 +112,7 @@ def main(parse_args=True):
 
     if d["file"][-1] != "s" and d["file"] not in ["back", "backburner"]:
         d["file"] += "s"
-    else:
+    elif d["file"] in ["back", "backburner"]:
         d["file"] = "backburner"
 
     if d["ref_proj"] == "ALL":
